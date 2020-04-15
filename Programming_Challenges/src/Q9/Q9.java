fe5a47042e11b8e8349b999c51841a3ba1c286a3
wkj1103 <wkj1103@naver.com>
2020-04-16 03:10:20 +0900

    Programming Challenges Q9. JollyJumpers

---
+++ Programming_Challenges/src/Q9/Q9.java
@@ -0,0 +1,35 @@
+package Q9;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ponyo
+ * Programming Challenges Q9. 유쾌한 점퍼(Jolly Jumpers)
+ *
+ */
+
+public class Q9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Solution solution = new Solution();
+		
+		while(sc.hasNextLine()) {
+			int n = sc.nextInt();
+			int[] inputSequence = new int[n];
+			
+			for(int i=0; i<n; i++) 
+				inputSequence[i] = sc.nextInt();
+			
+			solution.run(inputSequence);
+		}
+		
+		sc.close();
+
+	}
+
+}
