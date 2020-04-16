f4d0302322fea10664ac73a53c46c25822983db0
wkj1103 <wkj1103@naver.com>
2020-04-16 15:44:11 +0900

    Programming Challenges Q.13 Stack'em Up

---
+++ Programming_Challenges/src/Q13/Card.java
@@ -0,0 +1,60 @@
+package Q13;
+
+public class Card {
+	String[] cards;	// 카드
+	int[] order;	// 순서 번호
+	
+	public Card() {
+		cards = new String[52];
+		order = new int[52];
+		
+		cardInit();	
+		orderInit();	
+	}
+	
+	public void cardInit() {
+		
+		String[] nums = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+		String[] marks = {"Clubs", "Diamonds", "Hearts", "Spades"};
+		int i=0;
+		int index = 0;
+		
+		for(;i<13;i++)
+			this.cards[i] = nums[index++] + " of "+marks[0];
+		
+		index = 0;
+		for(;i<26;i++)
+			this.cards[i] = nums[index++] + " of "+marks[1];
+		
+		index = 0;
+		for(;i<39;i++)
+			this.cards[i] = nums[index++] + " of "+marks[2];
+
+		index = 0;
+		for(;i<52;i++)
+			this.cards[i] = nums[index++] + " of "+marks[3];
+	}
+	
+	public void orderInit() {
+		for(int i=0; i<52; i++) {
+			this.order[i] = i;
+		}
+	}
+	
+	public void mixCards(int[] pattern) {
+		// 주어진 패턴대로 순서번호를 섞음
+		int[] newOrder = new int[52];
+		
+		for(int i=0; i<pattern.length; i++)
+			newOrder[i] = this.order[pattern[i]];
+		
+		this.order = newOrder;
+	}
+
+	public void printCards() {
+		for(int i=0; i<this.order.length; i++)
+			System.out.println(this.cards[this.order[i]]);
+		
+		System.out.println();
+	}
+}
---
+++ Programming_Challenges/src/Q13/Main.java
@@ -0,0 +1,52 @@
+package Q13;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ponyo
+ * Programming Challenges Q13. Stack'em Up (쌓아 올리기)
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int testCaseNum = sc.nextInt();	// test case 개수
+		sc.nextLine();
+		sc.nextLine();
+		
+		for(int i=0; i<testCaseNum; i++) {
+			int n = sc.nextInt();	// 카드 섞기 패턴 개수
+			int[][] patterns = new int[n][52];
+			
+			for(int j=0; j<n; j++) {
+				for(int k=0; k<52; k++)
+					patterns[j][k] = sc.nextInt()-1;
+			}
+			sc.nextLine();
+			
+			Card card = new Card();	
+			while(true) {
+				String line = sc.nextLine();
+				
+				if(line.equals("")) {
+					card.printCards();
+					break;
+				}
+				else {
+					int patternNum = Integer.parseInt(line)-1;
+					card.mixCards(patterns[patternNum]);
+					if(!sc.hasNextLine()) {
+						card.printCards();
+						break;
+					}
+				}
+			}
+		}
+
+	}
+
+}
